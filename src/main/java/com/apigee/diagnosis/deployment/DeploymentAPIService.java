bc4686d40c047d463ae2ebfcd23f218736b64584
Senthil kumar Tamizhselvan <tskumar@apigee.com>
2016-09-06 11:41:59 +0530

    Commiting the build and changes in local url

--- src/main/java/com/apigee/diagnosis/deployment/DeploymentAPIService.java
+++ src/main/java/com/apigee/diagnosis/deployment/DeploymentAPIService.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
 /**
  * Created by senthil on 21/08/16.
  */
@@ -26,8 +25,11 @@ public class DeploymentAPIService {
     private String api;
     private String revision;
 
+    //TODO: Make it as a system property.
     private static final String MGMT = "api.enterprise.apigee.com";
     private static final String LOCAL_URL = "localhost:8080";
+
+    //TODO: Create an read-only sysadmin for apigee public cloud.
     private static final String USERNAME = "nagios@apigee.com";
     private static final String PASSWORD = "xnYbykQa7G";
 
@@ -40,19 +42,19 @@ public class DeploymentAPIService {
 
     private static Logger logger = LoggerFactory.getLogger(DeploymentAPIService.class);
 
-    public String getOrg(){
+    public String getOrg() {
         return org;
     }
 
-    public String getEnv(){
+    public String getEnv() {
         return env;
     }
 
-    public String getApi(){
+    public String getApi() {
         return api;
     }
 
-    public String getRevision(){
+    public String getRevision() {
         return revision;
     }
 
@@ -64,33 +66,38 @@ public class DeploymentAPIService {
     }
 
     public APIDeploymentState getDeploymentStatus(String org, String env, String api, String revision) {
-
         List<Server> servers = new ArrayList<Server>();
-
-        String mgmtResponse = getMgmtDeploymentStatusAPI(org,env,api,revision);
-        //Check Mgmt API Deployment Status
-        String mgmtStatus = checkMgmtStatus(mgmtResponse);
+        String mgmtResponse = getMgmtDeploymentStatusAPI(org, env, api, revision);
 
         //Fetch MP API Response
         String mpResponse = getMPDeploymentStatusAPI(org,env,api,revision);
 
-        //Get MP ServerList
-        List<String> mpServers = JSONResponseParser.getMessageProcessorList(mpResponse);
-        servers = setServerStatus(mpServers,mgmtResponse,mpResponse);
-        Revision revisionBean = new Revision(revision, (Server[]) servers.toArray());
-        return  new APIDeploymentState(getOrg(), getEnv(), getApi(), new Revision[]{revisionBean}, null);
+        //Check Mgmt API Deployment Status
+        String mgmtStatus = checkMgmtStatus(mgmtResponse);
+
+        if (mgmtStatus.equalsIgnoreCase(ERROR) || mgmtStatus.equalsIgnoreCase(MISSING)) {
+            servers = setServerStatusForErrorState(mgmtResponse, mpResponse);
+            Server allMPServers[] = servers.toArray(new Server[servers.size()]);
+            Revision revisionBean = new Revision(revision, allMPServers);
+            return new APIDeploymentState(getOrg(), getEnv(), getApi(), new Revision[]{revisionBean}, null);
+        } else {
+            servers = setServerStatusForSuccessState(mgmtStatus, mpResponse);
+            Server allMPServers[] = servers.toArray(new Server[servers.size()]);
+            Revision revisionBean = new Revision(revision, allMPServers);
+            return new APIDeploymentState(getOrg(), getEnv(), getApi(), new Revision[]{revisionBean}, null);
+        }
     }
 
-    private List<Server> setServerStatus(List<String> mpServers, String mgmtResponse, String mpResponse) {
+    private List<Server> setServerStatusForErrorState(String mgmtResponse, String mpResponse) {
         List<Server> servers = new ArrayList<Server>();
         try {
             JSONObject mgmtObj = new JSONObject(mgmtResponse);
             JSONArray serverObj1 = mgmtObj.getJSONArray("server");
             JSONObject mpObj = new JSONObject(mpResponse);
-            JSONObject revObj = (JSONObject) mpObj.getJSONArray("Revision").get(0);
-            JSONArray serverObj2 = revObj.getJSONArray("server");
-            for(String mpServer : mpServers) {
-                servers.add(processServerStatus(mpServer,serverObj1,serverObj2));
+            JSONObject revObj = (JSONObject) mpObj.getJSONArray("revision").get(0);
+            JSONArray serverObj2 = revObj.getJSONArray("servers");
+            for (String mpServer : JSONResponseParser.getMessageProcessorList(serverObj2)) {
+                servers.add(processServerStatusErrorState(mpServer, serverObj1, serverObj2));
             }
         } catch (JSONException e) {
             logger.error("Invalid JSON response");
@@ -99,20 +106,61 @@ public class DeploymentAPIService {
         return servers;
     }
 
-    private Server processServerStatus(String mpServer, JSONArray serverObj1, JSONArray serverObj2) throws JSONException {
+    private List<Server> setServerStatusForSuccessState(String mgmtStatus, String mpResponse) {
+        List<Server> servers = new ArrayList<Server>();
+        try {
+            JSONObject mpObj = new JSONObject(mpResponse);
+            JSONObject revObj = (JSONObject) mpObj.getJSONArray("revision").get(0);
+            JSONArray serverObj2 = revObj.getJSONArray("servers");
+            for (String mpServer : JSONResponseParser.getMessageProcessorList(serverObj2)) {
+                servers.add(processServerStatusSuccessState(mpServer, mgmtStatus, serverObj2));
+            }
+        } catch (JSONException e) {
+            logger.error("Invalid JSON response");
+            e.printStackTrace();
+        }
+        return servers;
+    }
+
+    private Server processServerStatusErrorState(String mpServer, JSONArray serverObj1, JSONArray serverObj2) throws JSONException {
         String status = new String();
-        for(int i=0;i<serverObj1.length();i++) {
-            if (serverObj1.getJSONObject(i).getString("uUID") == mpServer) {
+        for (int i = 0; i < serverObj1.length(); i++) {
+            if (serverObj1.getJSONObject(i).getString("uUID").equalsIgnoreCase(mpServer)) {
                 int j = 0;
-                while(j<serverObj2.length()) {
-                    if(serverObj2.getJSONObject(j).getString("uUID") == mpServer){
-                        status = serverObj1.getJSONObject(i).getString("status") == serverObj2.getJSONObject(j).getString("status") ? SYNC : MISMATCH;
+                while (j < serverObj2.length()) {
+                    if (serverObj2.getJSONObject(j).getString("uuid").equalsIgnoreCase(mpServer)) {
+                        String mpStatus = serverObj2.getJSONObject(j).getString("status").equalsIgnoreCase("success") ? DEPLOYED : UNDEPLOYED;
+                        String mgmtStatus = serverObj1.getJSONObject(i).getString("status");
+                        status = mgmtStatus.equalsIgnoreCase(mpStatus) ? SYNC : MISMATCH;
+                        String errorMessage = null;
+                        if (status == MISMATCH) {
+                            errorMessage = "Ideal State:" + mgmtStatus + " MP State:" + mpStatus;
+                        }
+                        return new Server(mpServer, status, null, errorMessage);
                     }
                     j++;
                 }
             }
         }
-        return new Server(mpServer,status,null,null);
+        return null;
+    }
+
+    private Server processServerStatusSuccessState(String mpServer, String mgmtStatus, JSONArray serverObj2) throws JSONException {
+        String status = new String();
+        int j = 0;
+        while (j < serverObj2.length()) {
+            if (serverObj2.getJSONObject(j).getString("uuid").equalsIgnoreCase(mpServer)) {
+                String mpStatus = serverObj2.getJSONObject(j).getString("status").equalsIgnoreCase("success") ? DEPLOYED : UNDEPLOYED;
+                status = mgmtStatus.equalsIgnoreCase(mpStatus) ? SYNC : MISMATCH;
+                String errorMessage = null;
+                if (status == MISMATCH) {
+                    errorMessage = "Ideal State:" + mgmtStatus + " MP State:" + mpStatus;
+                }
+                return new Server(mpServer, status, null, errorMessage);
+            }
+            j++;
+        }
+        return null;
     }
 
     private String checkMgmtStatus(String jsonResponse) {
@@ -126,32 +174,33 @@ public class DeploymentAPIService {
         return null;
     }
 
-    private String getMgmtDeploymentStatusAPI(String org, String env,String api, String revision) {
+    private String getMgmtDeploymentStatusAPI(String org, String env, String api, String revision) {
         String mgmtAPIDeployURL = new String();
         String apiDeploymentStatus = new String();
-        if(revision == null){
-            mgmtAPIDeployURL = "https://"+MGMT+"/v1/o/"+org+"/e/"+env+"/apis/"+api+"/deployments";
+        if (revision == null) {
+            mgmtAPIDeployURL = "https://" + MGMT + "/v1/o/" + org + "/e/" + env + "/apis/" + api + "/deployments";
         } else {
-            mgmtAPIDeployURL = "https://"+MGMT+"/v1/o/"+org+"/e/"+env+"/apis/"+api+"/revisions/"+revision+"/deployments";
+            mgmtAPIDeployURL = "https://" + MGMT + "/v1/o/" + org + "/e/" + env + "/apis/" + api + "/revisions/" + revision + "/deployments";
         }
         try {
-            apiDeploymentStatus = RestAPIExecutor.executeGETAPI(mgmtAPIDeployURL,USERNAME+":"+PASSWORD, "json");
+            apiDeploymentStatus = RestAPIExecutor.executeGETAPI(mgmtAPIDeployURL, USERNAME + ":" + PASSWORD, "json");
         } catch (Exception e) {
             apiDeploymentStatus = "{ }";
         }
         return apiDeploymentStatus;
     }
 
-    private String getMPDeploymentStatusAPI(String org, String env,String api, String revision){
+    private String getMPDeploymentStatusAPI(String org, String env, String api, String revision) {
         String mpAPIDeployURL = new String();
         String apiDeploymentStatus = new String();
-        if(revision == null){
-            mpAPIDeployURL = "https://"+LOCAL_URL+"/v1/o/"+org+"/e/"+env+"/apis/"+api+"/deployments";
+        if (revision == null) {
+            mpAPIDeployURL = "http://" + LOCAL_URL + "/v1/diagnosis/mpdeploymentinfo/organizations/" + org + "/environments/" + env + "/apis/" + api + "/status";
         } else {
-            mpAPIDeployURL = "https://"+LOCAL_URL+"/v1/o/"+org+"/e/"+env+"/apis/"+api+"/revisions/"+revision+"/deployments";
+            mpAPIDeployURL = "http://" + LOCAL_URL + "/v1/diagnosis/mpdeploymentinfo/organizations/" + org + "/environments/" + env + "/apis/" + api + "/revisions/" + revision + "/status";
         }
         try {
-            apiDeploymentStatus = RestAPIExecutor.executeGETAPI(mpAPIDeployURL,USERNAME+":"+PASSWORD, "json");
+            logger.info(mpAPIDeployURL);
+            apiDeploymentStatus = RestAPIExecutor.executeGETAPI(mpAPIDeployURL, USERNAME + ":" + PASSWORD, "json");
         } catch (Exception e) {
             apiDeploymentStatus = "{ }";
         }
--- src/main/java/com/apigee/diagnosis/util/JSONResponseParser.java
+++ src/main/java/com/apigee/diagnosis/util/JSONResponseParser.java
@@ -2,7 +2,8 @@ package com.apigee.diagnosis.util;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,15 +12,20 @@ import java.util.List;
  * Created by senthil on 24/08/16.
  */
 public class JSONResponseParser {
-    public static List<String> getMessageProcessorList(String jsonResponse) {
+    private static Logger logger = LoggerFactory.getLogger(JSONResponseParser.class);
+
+    public static List<String> getMessageProcessorList(JSONArray mpservers) {
         List<String> servers = new ArrayList<String>();
         try {
-            JSONObject jsonObject = new JSONObject(jsonResponse);
-            JSONArray serversObj = jsonObject.getJSONArray("server");
-                for(int i=0;i<serversObj.length();i++) {
-                    servers.add(serversObj.getJSONObject(i).getString("uUID"));
-                }
+            if (mpservers.length() == 0){
+                logger.error("No Servers");
+                return servers;
+            }
+            for(int i=0;i<mpservers.length();i++) {
+               servers.add(mpservers.getJSONObject(i).getString("uuid"));
+            }
         } catch (JSONException e) {
+            logger.error("Invalid JSON Message");
             return servers;
         }
         return servers;
