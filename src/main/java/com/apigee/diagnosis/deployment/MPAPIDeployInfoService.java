fdd5a864a291499fcd4cc0e34c825137cc2bfb62
Amar Devegowda <adevegowda@apigee.com>
2016-09-16 16:20:05 +0530

    Re-organize the  MP Deployment Service Code to make it more reusable and modularized

--- src/main/java/com/apigee/diagnosis/deployment/MPAPIDeployInfoService.java
+++ src/main/java/com/apigee/diagnosis/deployment/MPAPIDeployInfoService.java
@@ -3,6 +3,7 @@ package com.apigee.diagnosis.deployment;
 import com.apigee.diagnosis.beans.APIDeploymentState;
 import com.apigee.diagnosis.beans.Revision;
 import com.apigee.diagnosis.beans.Server;
+import com.apigee.diagnosis.mp.MPManager;
 import com.apigee.diagnosis.util.RestAPIExecutor;
 import com.apigee.diagnosis.util.XMLResponseParser;
 import com.apigee.diagnosis.zk.ZKClientManager;
@@ -27,37 +28,14 @@ public class MPAPIDeployInfoService {
     private String api;
     private String revision;
 
-    //
-    private boolean inputValid = false;
-
-    // instance of ZKClientManager
-    private ZKClientManager zkClientManager;
-
-    // czk01apigee (US EAST), czk04apigee (US WEST), czk060sy (AP SOUTHEAST), czk21eu (EU)
-    private final static String ZK_SERVER_ENSEMBLE = "192.168.11.13,192.168.41.159,10.10.30.17,192.168.73.241";
-    //private final static String ZK_SERVER_ENSEMBLE = initializeZKHosts();
-
-    // Znode names
-    private final static String REGIONS_NODE = "regions";
-    private final static String PODS_NODE ="pods";
-    private final static String TYPES_NODE = "types";
-    private final static String MESSAGE_PROCESSOR_NODE = "message-processor";
-    private final static String SERVERS_NODE = "servers";
-    private final static String INTERNAL_IP_NODE = "InternalIP";
-    private final static String EXTERNAL_IP_NODE = "ExternalIP";
-    private final static String EXTERNAL_HOSTNAME_NODE = "ExternalHostName";
-
     private final static String V1 = "v1";
     private final static String RUNTIME = "runtime";
     private final static String MP_PORT_NUMBER = "8080";
-    // LOG instance
-    private static Logger LOG = LoggerFactory.getLogger(ZKAPIDeployInfoService.class);
 
-    private static String initializeZKHosts() {
-        String zkHosts = System.getProperty("zookeeper.hosts");
-        LOG.info("ZK Hosts = " + zkHosts);
-        return zkHosts;
-    }
+    private MPManager mpManager;
+
+    // LOG instance
+    private static Logger LOG = LoggerFactory.getLogger(MPAPIDeployInfoService.class);
 
     public String getOrg(){
         return org;
@@ -75,10 +53,6 @@ public class MPAPIDeployInfoService {
         return revision;
     }
 
-    private boolean isInputValid() {
-        return inputValid;
-    }
-
     public MPAPIDeployInfoService(String org, String env, String api)
             throws KeeperException, IOException, InterruptedException {
         if (org == null || env == null || api == null) {
@@ -89,20 +63,7 @@ public class MPAPIDeployInfoService {
         this.org = org;
         this.env = env;
         this.api = api;
-
-        // open the connection with Zookeeper Ensemble
-        initialize();
-
-        try {
-            // validate the arguments
-            validateInputArguments();
-        } catch (InterruptedException ie) {
-            close();
-            throw ie;
-        } catch (IllegalArgumentException iae) {
-            close();
-            throw iae;
-        }
+        mpManager = new MPManager();
     }
 
     public MPAPIDeployInfoService(String org, String env,
@@ -118,35 +79,14 @@ public class MPAPIDeployInfoService {
         this.env = env;
         this.api = api;
         this.revision = revision;
-
-        // open the connection with Zookeeper Ensemble
-        initialize();
-
-        // validate the arguments
-        try {
-            validateInputArguments();
-        } catch (InterruptedException ie) {
-            close();
-            throw ie;
-        } catch (IllegalArgumentException iae) {
-            close();
-            throw iae;
-        }
-    }
-
-    /*
-     *  Initializes the connection with Zookeeper Ensemble
-     */
-    private void initialize() throws IOException, InterruptedException {
-        zkClientManager = new ZKClientManager(ZK_SERVER_ENSEMBLE);
-        zkClientManager.openConnection();
+        mpManager = new MPManager();
     }
 
     /*
      * Checks if all the input arguments are valid
      *
      */
-    private void validateInputArguments() throws KeeperException,
+/*    private void validateInputArguments() throws KeeperException,
             InterruptedException, IllegalArgumentException {
 
         LOG.info("Validating Input Arguments");
@@ -175,167 +115,7 @@ public class MPAPIDeployInfoService {
 
         inputValid = true;
         LOG.info("Validation of Input Arguments is complete - Input Arguments are valid");
-    }
-
-    /*
-     * Gets all the regions for the specific organization and environment
-     * set in this class
-     * regions node path:
-     *     /organizations/<org>/environments/<env>/regions
-     */
-    private List<String> getAllRegionsForOrgAndEnv() throws KeeperException,
-            InterruptedException {
-        LOG.info("Fetching the regions");
-
-        String orgEnvRegionsPath = "/organizations/" + org +
-                "/environments/" + env +
-                "/" + REGIONS_NODE;
-        List<String> regions = zkClientManager.getZNodeChildren(orgEnvRegionsPath);
-
-        LOG.info("Regions retrieved " + regions);
-        return regions;
-    }
-
-    /*
-     * Gets all the regions for the specific organization and environment
-     * set in this class
-     * pods node path:
-     *     /organizations/<org>/environments/<env>/regions/<region>/pods
-     */
-    private List<String> getPodsInRegion(String region) throws KeeperException,
-            InterruptedException {
-        LOG.info("Fetching the pods in region " + region);
-
-        String orgEnvPodsPath = "/organizations/" + org +
-                "/environments/" + env +
-                "/" + REGIONS_NODE + "/" + region +
-                "/" + PODS_NODE;
-        List<String> pods = zkClientManager.getZNodeChildren(orgEnvPodsPath);
-
-
-        LOG.info("Pods retrieved " + pods);
-        return pods;
-    }
-
-    /*
-     * Gets all the regions and pods for the specific organization and environment
-     * set in this class
-     */
-    private Map<String, List<String>> getRegionsAndPodsForOrgAndEnv() throws KeeperException,
-            InterruptedException {
-        List <String> regions = getAllRegionsForOrgAndEnv();
-        if (regions == null) {
-            return null;
-        }
-
-        Map<String, List<String>> regionsAndPodsMap = new HashMap<String, List<String>>();
-        for(String region: regions) {
-            List<String> pods = getPodsInRegion(region);
-            regionsAndPodsMap.put(region, pods);
-        }
-        return regionsAndPodsMap;
-    }
-
-    /*
-     * Gets all the regions for the specific organization and environment
-     * set in this class
-     * pods node path:
-     *     /organizations/<org>/environments/<env>/regions/<region>/pods
-     */
-    private List<String> getMPServerUUIDsInPod(String region, String pod) throws KeeperException,
-            InterruptedException {
-        LOG.info("Fetching the MPs in pod " + pod);
-
-        String messageProcessorPath = "/" + REGIONS_NODE + "/" + region +
-                "/" + PODS_NODE + "/" + pod +
-                "/" + TYPES_NODE +
-                "/" + MESSAGE_PROCESSOR_NODE;
-        List<String> mpServerUUIDs = zkClientManager.getZNodeChildren(messageProcessorPath);
-
-        LOG.info("MPs retrieved " + mpServerUUIDs);
-        return mpServerUUIDs;
-    }
-
-    /*
-     * Gets the InternalIP for the specific MP
-     * InternalIP node path:
-     *     /regions/<region-name>/pods/<pod-name>/servers/<server-uuid>/InternalIP
-     */
-    private String getMPInternalIP(String region, String pod,
-                                   String mpUUID) throws KeeperException,
-            InterruptedException {
-        LOG.info("Fetching the Internal IP address of the MP " + mpUUID);
-
-        String internalIPPath = "/" + REGIONS_NODE + "/" + region +
-                "/" + PODS_NODE + "/" + pod +
-                "/" + SERVERS_NODE + "/" + mpUUID +
-                "/" + INTERNAL_IP_NODE;
-        String internalIP = zkClientManager.getZNodeData(internalIPPath);
-
-        LOG.info("MP Internal IP retrieved");
-        return internalIP;
-    }
-
-    /*
-     * Gets the ExternalIP for the specific MP
-     * ExternalIP node path:
-     *     /regions/<region-name>/pods/<pod-name>/servers/<server-uuid>/ExternalIP
-     */
-    private String getMPExternalIP(String region, String pod,
-                                   String mpUUID) throws KeeperException,
-            InterruptedException {
-        LOG.info("Fetching the External IP address of the MP " + mpUUID);
-
-        String externalIPPath = "/" + REGIONS_NODE + "/" + region +
-                "/" + PODS_NODE + "/" + pod +
-                "/" + SERVERS_NODE + "/" + mpUUID +
-                "/" + EXTERNAL_IP_NODE;
-        String externalIP = zkClientManager.getZNodeData(externalIPPath);
-
-        LOG.info("MP External IP retrieved");
-        return externalIP;
-    }
-
-    /*
-     * Gets the ExternalHostName for the specific MP
-     * ExternalIP node path:
-     *     /regions/<region-name>/pods/<pod-name>/servers/<server-uuid>/ExternalHostName
-     */
-    private String getMPExternalHostName(String region, String pod,
-                                   String mpUUID) throws KeeperException,
-            InterruptedException {
-        LOG.info("Fetching the External HostName of the MP " + mpUUID);
-
-        String externalIPPath = "/" + REGIONS_NODE + "/" + region +
-                "/" + PODS_NODE + "/" + pod +
-                "/" + SERVERS_NODE + "/" + mpUUID +
-                "/" + EXTERNAL_HOSTNAME_NODE;
-        String externalIP = zkClientManager.getZNodeData(externalIPPath);
-
-        LOG.info("MP External HostName retrieved");
-        return externalIP;
-    }
-
-    /*
-     * Gets all the revisions of the API Proxy for the specific organization
-     * and environment set in this class
-     * revisions node path:
-     *     /organizations/<org>/environments/<env>/apis/<api>/regions
-     */
-    private List<String> getAllRevisionsOfAPIForOrgAndEnv() throws KeeperException,
-            InterruptedException {
-        LOG.info("Fetching the revisions");
-
-        String revisionsPath = "/organizations/" + org +
-                "/environments/" + env +
-                "/apiproxies/" + api +
-                "/revisions";
-        List<String> revisions = zkClientManager.getZNodeChildren(revisionsPath);
-
-        LOG.info("Revisions retrieved " + revisions);
-        return revisions;
-    }
-
+    }*/
 
     /*
      * Gets the deployed revision of the specific API on MP
@@ -373,7 +153,8 @@ public class MPAPIDeployInfoService {
      */
     public APIDeploymentState getCompleteDeploymentInfoOnAllMPs() throws IOException,
             KeeperException, InterruptedException, Exception {
-        Map<String, List<String>> regionsAndPodsMap = getRegionsAndPodsForOrgAndEnv();
+        Map<String, List<String>> regionsAndPodsMap =
+                mpManager.getRegionsAndPodsForOrgAndEnv(getOrg(), getEnv());
 
         if (regionsAndPodsMap == null) {
             return null;
@@ -386,13 +167,13 @@ public class MPAPIDeployInfoService {
         for(Entry<String, List<String>> entry : regionsAndPodsMap.entrySet()){
             String region = entry.getKey();
             for (String pod: entry.getValue()) {
-                List <String> mpsUUIDs = getMPServerUUIDsInPod(region, pod);
+                List <String> mpsUUIDs = mpManager.getMPServerUUIDsInPod(region, pod);
                 // For each of the MP servers, fetch the internal and external IP addresses
                 // and deployed revision of the API on the MP
                 for (String mpUUID: mpsUUIDs) {
-                    String internalIP = getMPInternalIP(region, pod, mpUUID);
-                    String externalIP = getMPExternalIP(region, pod, mpUUID);
-                    String externalHostName = getMPExternalHostName(region, pod, mpUUID);
+                    String internalIP = mpManager.getMPInternalIP(region, pod, mpUUID);
+                    String externalIP = mpManager.getMPExternalIP(region, pod, mpUUID);
+                    String externalHostName = mpManager.getMPExternalHostName(region, pod, mpUUID);
 
                     // Run the API on the MP to get the revision deployed for the specific API
                     String xmlResponse = getAPIDeployedRevisionOnMP(externalIP);
@@ -445,7 +226,8 @@ public class MPAPIDeployInfoService {
      */
     public APIDeploymentState getCompleteDeploymentInfoOnAllMPsForAllRevisions() throws IOException,
             KeeperException, InterruptedException, Exception {
-        Map<String, List<String>> regionsAndPodsMap = getRegionsAndPodsForOrgAndEnv();
+        Map<String, List<String>> regionsAndPodsMap =
+                mpManager.getRegionsAndPodsForOrgAndEnv(getOrg(), getEnv());
 
         if (regionsAndPodsMap == null) {
             return null;
@@ -453,7 +235,7 @@ public class MPAPIDeployInfoService {
 
         // Get all the revisions of the api for the specific org and env
         // from the Zookeeper
-        List<String> allRevisions = getAllRevisionsOfAPIForOrgAndEnv();
+        List<String> allRevisions = mpManager.getAllRevisionsOfAPIForOrgAndEnv(getOrg(), getEnv(), getApi());
 
         if (allRevisions == null) {
             return null;
@@ -470,13 +252,13 @@ public class MPAPIDeployInfoService {
         for(Entry<String, List<String>> entry : regionsAndPodsMap.entrySet()){
             String region = entry.getKey();
             for (String pod: entry.getValue()) {
-                List <String> mpsUUIDs = getMPServerUUIDsInPod(region, pod);
+                List <String> mpsUUIDs = mpManager.getMPServerUUIDsInPod(region, pod);
                 // For each of the MP servers, fetch the internal, external IP addresses
                 // external HostName and deployed revision of the API on the MP
                 for (String mpUUID: mpsUUIDs) {
-                    String internalIP = getMPInternalIP(region, pod, mpUUID);
-                    String externalIP = getMPExternalIP(region, pod, mpUUID);
-                    String externalHostName = getMPExternalHostName(region, pod, mpUUID);
+                    String internalIP = mpManager.getMPInternalIP(region, pod, mpUUID);
+                    String externalIP = mpManager.getMPExternalIP(region, pod, mpUUID);
+                    String externalHostName = mpManager.getMPExternalHostName(region, pod, mpUUID);
 
                     // Run the API on the MP to get the revisions deployed for the specific API
                     String xmlResponse = getAPIDeployedRevisionOnMP(externalIP);
@@ -522,7 +304,6 @@ public class MPAPIDeployInfoService {
     }
 
     public void close() throws InterruptedException {
-        zkClientManager.closeConnection();
+        mpManager.closeZKConnection();
     }
-
 }
---
+++ src/main/java/com/apigee/diagnosis/mp/MPManager.java
@@ -0,0 +1,220 @@
+package com.apigee.diagnosis.mp;
+
+import com.apigee.diagnosis.deployment.ZKAPIDeployInfoService;
+import com.apigee.diagnosis.zk.ZKClientManager;
+import org.apache.zookeeper.KeeperException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+
+/**
+ * Created by amar on 16/09/16.
+ */
+public class MPManager {
+
+    // instance of ZKClientManager
+    private ZKClientManager zkClientManager;
+
+    // czk01apigee (US EAST), czk04apigee (US WEST), czk060sy (AP SOUTHEAST), czk21eu (EU)
+    private final static String ZK_SERVER_ENSEMBLE = "192.168.11.13,192.168.41.159,10.10.30.17,192.168.73.241";
+    //private final static String ZK_SERVER_ENSEMBLE = initializeZKHosts();
+
+
+    // Znode names
+    private final static String REGIONS_NODE = "regions";
+    private final static String PODS_NODE ="pods";
+    private final static String TYPES_NODE = "types";
+    private final static String MESSAGE_PROCESSOR_NODE = "message-processor";
+    private final static String SERVERS_NODE = "servers";
+    private final static String INTERNAL_IP_NODE = "InternalIP";
+    private final static String EXTERNAL_IP_NODE = "ExternalIP";
+    private final static String EXTERNAL_HOSTNAME_NODE = "ExternalHostName";
+
+    // LOG instance
+    private static Logger LOG = LoggerFactory.getLogger(MPManager.class);
+
+    public MPManager() throws IOException, InterruptedException {
+        openZKConnection();
+    }
+
+    /*
+     *  Initializes the connection with Zookeeper Ensemble
+     */
+    private void openZKConnection() throws IOException, InterruptedException {
+        zkClientManager = new ZKClientManager(ZK_SERVER_ENSEMBLE);
+        zkClientManager.openConnection();
+    }
+
+    /*
+     * Gets all the regions for the specific organization and environment
+     * set in this class
+     * regions node path:
+     *     /organizations/<org>/environments/<env>/regions
+     */
+    public List<String> getAllRegionsForOrgAndEnv(String org, String env) throws KeeperException,
+            InterruptedException {
+        LOG.info("Fetching the regions");
+
+        String orgEnvRegionsPath = "/organizations/" + org +
+                "/environments/" + env +
+                "/" + REGIONS_NODE;
+        List<String> regions = zkClientManager.getZNodeChildren(orgEnvRegionsPath);
+
+        LOG.info("Regions retrieved " + regions);
+        return regions;
+    }
+
+    /*
+     * Gets all the regions for the specific organization and environment
+     * set in this class
+     * pods node path:
+     *     /organizations/<org>/environments/<env>/regions/<region>/pods
+     */
+    public List<String> getPodsInRegion(String org, String env, String region) throws KeeperException,
+            InterruptedException {
+        LOG.info("Fetching the pods in region " + region);
+
+        String orgEnvPodsPath = "/organizations/" + org +
+                "/environments/" + env +
+                "/" + REGIONS_NODE + "/" + region +
+                "/" + PODS_NODE;
+        List<String> pods = zkClientManager.getZNodeChildren(orgEnvPodsPath);
+
+
+        LOG.info("Pods retrieved " + pods);
+        return pods;
+    }
+
+    /*
+     * Gets all the regions and pods for the specific organization and environment
+     * set in this class
+     */
+    public Map<String, List<String>> getRegionsAndPodsForOrgAndEnv(String org,
+                                                                   String env) throws KeeperException,
+            InterruptedException {
+        List <String> regions = getAllRegionsForOrgAndEnv(org, env);
+        if (regions == null) {
+            return null;
+        }
+
+        Map<String, List<String>> regionsAndPodsMap = new HashMap<String, List<String>>();
+        for(String region: regions) {
+            List<String> pods = getPodsInRegion(org, env, region);
+            regionsAndPodsMap.put(region, pods);
+        }
+        return regionsAndPodsMap;
+    }
+
+    /*
+     * Gets the UUIDs of all the MP Servers in a specific pod of a region
+     * pods node path:
+     *     /organizations/<org>/environments/<env>/regions/<region>/pods
+     */
+    public List<String> getMPServerUUIDsInPod(String region,
+                                              String pod) throws KeeperException,
+            InterruptedException {
+        LOG.info("Fetching the MPs in pod " + pod);
+
+        String messageProcessorPath = "/" + REGIONS_NODE + "/" + region +
+                "/" + PODS_NODE + "/" + pod +
+                "/" + TYPES_NODE +
+                "/" + MESSAGE_PROCESSOR_NODE;
+        List<String> mpServerUUIDs = zkClientManager.getZNodeChildren(messageProcessorPath);
+
+        LOG.info("MPs retrieved " + mpServerUUIDs);
+        return mpServerUUIDs;
+    }
+
+    /*
+     * Gets the InternalIP for the specific MP
+     * InternalIP node path:
+     *     /regions/<region-name>/pods/<pod-name>/servers/<server-uuid>/InternalIP
+     */
+    public String getMPInternalIP(String region, String pod,
+                                   String mpUUID) throws KeeperException,
+            InterruptedException {
+        LOG.info("Fetching the Internal IP address of the MP " + mpUUID);
+
+        String internalIPPath = "/" + REGIONS_NODE + "/" + region +
+                "/" + PODS_NODE + "/" + pod +
+                "/" + SERVERS_NODE + "/" + mpUUID +
+                "/" + INTERNAL_IP_NODE;
+        String internalIP = zkClientManager.getZNodeData(internalIPPath);
+
+        LOG.info("MP Internal IP retrieved");
+        return internalIP;
+    }
+
+    /*
+     * Gets the ExternalIP for the specific MP
+     * ExternalIP node path:
+     *     /regions/<region-name>/pods/<pod-name>/servers/<server-uuid>/ExternalIP
+     */
+    public String getMPExternalIP(String region, String pod,
+                                   String mpUUID) throws KeeperException,
+            InterruptedException {
+        LOG.info("Fetching the External IP address of the MP " + mpUUID);
+
+        String externalIPPath = "/" + REGIONS_NODE + "/" + region +
+                "/" + PODS_NODE + "/" + pod +
+                "/" + SERVERS_NODE + "/" + mpUUID +
+                "/" + EXTERNAL_IP_NODE;
+        String externalIP = zkClientManager.getZNodeData(externalIPPath);
+
+        LOG.info("MP External IP retrieved");
+        return externalIP;
+    }
+
+    /*
+     * Gets the ExternalHostName for the specific MP
+     * ExternalHostName node path:
+     *     /regions/<region-name>/pods/<pod-name>/servers/<server-uuid>/ExternalHostName
+     */
+    public String getMPExternalHostName(String region, String pod,
+                                         String mpUUID) throws KeeperException,
+            InterruptedException {
+        LOG.info("Fetching the External HostName of the MP " + mpUUID);
+
+        String externalIPPath = "/" + REGIONS_NODE + "/" + region +
+                "/" + PODS_NODE + "/" + pod +
+                "/" + SERVERS_NODE + "/" + mpUUID +
+                "/" + EXTERNAL_HOSTNAME_NODE;
+        String externalIP = zkClientManager.getZNodeData(externalIPPath);
+
+        LOG.info("MP External HostName retrieved");
+        return externalIP;
+    }
+
+    /*
+     * Gets all the revisions of the API Proxy for the specific organization
+     * and environment set in this class
+     * revisions node path:
+     *     /organizations/<org>/environments/<env>/apis/<api>/regions
+     */
+    public List<String> getAllRevisionsOfAPIForOrgAndEnv(String org,
+                                                         String env,
+                                                         String api) throws KeeperException,
+            InterruptedException {
+        LOG.info("Fetching the revisions");
+
+        String revisionsPath = "/organizations/" + org +
+                "/environments/" + env +
+                "/apiproxies/" + api +
+                "/revisions";
+        List<String> revisions = zkClientManager.getZNodeChildren(revisionsPath);
+
+        LOG.info("Revisions retrieved " + revisions);
+        return revisions;
+    }
+
+    public void closeZKConnection() throws InterruptedException {
+        zkClientManager.closeConnection();
+    }
+
+}
